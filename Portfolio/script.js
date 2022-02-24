5ac8bd2de552af6a38dfec023f04c97c86dce596
oMelonz <josh28kim@gmail.com>
2022-02-24 22:30:50 +1100

    Mobile screen fit
    screen fit input

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -2,3 +2,8 @@ window.addEventListener('scroll', function(){
     var header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 0);
 })
+
+function toggleMenu(){
+    var menuToggle = document.querySelector('.toggle');
+    menuToggle.classList.toggle('active')
+}
